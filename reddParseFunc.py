f6486f5ba039ef01b5bf4fbe52540e453025fcf4
G3Dx5 <srfb2@live.com>
2022-09-03 13:59:05 +1000

    Update reddParseFunc.py
    Updated to python3

--- reddParseFunc.py
+++ reddParseFunc.py
@@ -1,52 +1,47 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-###############################################################
-# Script to parse feeds based on search from subreddit        #
-# By: GaDayas                                                 #
-# Date: 09FEB19                                               #
-# Version:  1.0                                               #
-###############################################################
+'''
+Parse feeds based on search from subreddit        
+Author: G3Dx5                                                   
+Date: SEP2022                                              
+Version:  1.1                                               
+'''
 
 import feedparser
-import time
 import sys
+import time
 
-# =============================================================
+
 # Set command line arguments for the subsequent search term
-# =============================================================
-
 if len(sys.argv) != 2:
-    print 'ERROR: You must specify a search term eg: ./script <searchterm>'
+    print('ERROR: You must specify a search term eg: python3 reddParseFunc.py <searchterm>')
     sys.exit(0)
 
 redprefix = 'http://www.reddit.com/r/'
 searterm = sys.argv[1]
 exten = '.rss'
 
-# ==============================================================
 # Parse the feed. Subredit is '/r' by default, can be changed
-# ==============================================================
-
 r = feedparser.parse(redprefix+searterm+exten)
 #print r.entries
-print r['feed']['title']
-print r.feed.subtitle
+print(r['feed']['title'])
+print(r.feed.subtitle)
 
 # Print out search time / date
-print "\n"
-print "================================================="
-print "Search Time:", time.strftime("%A %d %B %Y %I:%M %p")
-print "=================================================\n"
+print("\n")
+print("=================================================")
+print("Search Time:", time.strftime("%A %d %B %Y %I:%M %p"))
+print("=================================================\n")
 
-def runparse():
+def runparse() -> None:
     count = 1
     if count % 100 == 1:
         for post in r.entries:
-            print post.title
-            print post.link
-            print post.date
+            print(post.title)
+            print(post.link)
+            print(post.date)
             try:
-                print "Author:", post.author + "\n"
+                print("Author:", post.author + "\n")
             except:
                 continue
             count += 1
